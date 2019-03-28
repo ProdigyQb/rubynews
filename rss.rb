a08971214e59eba85fd47ebbdbd3bdd1fdcfd73b
ProdigyQb <48689131+ProdigyQb@users.noreply.github.com>
2019-03-28 23:53:03 +0100

    added .yml config file
    minor changes

--- rss.rb
+++ rss.rb
@@ -1,16 +1,21 @@
+require 'yaml'
 require "rss"
 require "open-uri"
-rsslinks = ["https://www.heise.de/autos/rss/news-atom.xml","https://www.heise.de/rss/heiseplus-atom.xml","https://www.giga.de/software/feed/","https://www.theverge.com/rss/index.xml"]
-i = 0
 
-for i in 0..6
-  open("#{rsslinks[i]}") do |rss|
+i = 0 #Counter for RSS-Feed Parser
+z = 0 #Counter for amout of yaml links to Pars
+limit = 8 # Limit for the amount of usable links
+config = YAML.load_file('C:\Users\Vsirt\Documents\git\rubynews\rubynews\config.yml')
+
+while z < limit do
+  z = z + 1
+  open("#{config["links"][z]}") do |rss|
     feed = RSS::Parser.parse(rss)
   puts 'Title:'+ ' #{feed.channel.title}'
     feed.items.each do |item|
   puts "Item: #{item.title}"
-  i = i + 1
+      i = i + 1
     end
-  end
-  puts "!!!!!!!!!!!!!!! RSS FEED COMING !!!!!!!!!!!!!!!"
+    end
+puts "!!!!!!!!!!!!!!! RSS FEED COMING !!!!!!!!!!!!!!!"
 end
